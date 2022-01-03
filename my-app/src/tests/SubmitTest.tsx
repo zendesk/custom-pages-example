458b1baa7922120f284f02b13fa6293b84efc442
Erica Girges <erica.girges@zendesk.com>
2022-01-03 13:21:12 -0800

    will make sure to work off of branches for future PR's

--- my-app/src/tests/SubmitTest.tsx
+++ my-app/src/tests/SubmitTest.tsx
@@ -1,1 +1,9 @@
-// unit testing will happen here
+import React from 'react';
+// need to import jest
+
+function SubmitTest() {
+
+    // unit testing for submit request to backend here
+}
+
+export default SubmitTest;
