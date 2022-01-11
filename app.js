df517ce0f126c4d8e202fe9ef15fd3b6b5f3efe3
tipenehughes <tipenehughes@gmail.com>
2022-01-11 18:24:58 -0500

    Refactored to use create_or_update user endpoint. Added functionality to add multiple orgs in case of an org already being on the user profile

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const fetch = require("node-fetch");
 require("dotenv").config();
 
@@ -13,93 +14,118 @@ const encodedData = Buffer.from(
 ).toString("base64");
 
 const subdomain = process.env.SUBDOMAIN;
-const headers = {
+
+const httpHeaders = {
   "Content-Type": "application/json",
   Authorization: `Basic ${encodedData}`,
 };
+const headers = new fetch.Headers(httpHeaders);
 
 // Function to create org if one does not already exist
 const createOrg = async (req) => {
   const url = `https://${subdomain}.zendesk.com/api/v2/organizations`;
   const config = {
     method: "POST",
-    headers: headers,
+    headers,
     body: JSON.stringify({
       organization: { name: `${req.body.organization}` },
     }),
   };
-
-  const response = await fetch(url, config);
-
-  if (response.ok) {
-    console.log("Org successfully created");
-  } else {
-    response.json().then((data) => console.log(data.details));
-  }
-};
-
-// Function to check if user exists
-const checkUser = async (req, res) => {
-  const url = `https://${subdomain}.zendesk.com/api/v2/users/search.json?query=type:user "${req.email}"`;
-  const config = {
-    headers: headers,
-  };
+  // Call to create org
   const response = await fetch(url, config);
   const data = await response.json();
 
-  return {
-    users: data.users,
-    req,
-    res,
+  // Function to get organization ID if org already exists
+  const getExistingOrg = async () => {
+    const checkOrg = `https://${subdomain}.zendesk.com/api/v2/search.json?query=type:organization "${req.body.organization}"`;
+    const response = await fetch(checkOrg, { headers });
+    const data = await response.json();
+    console.log(
+      `An organization with the name ${req.body.organization} already exists. The organization will be added to the users profile.`
+    );
+    // Searches org data array for exact match with organization name and returns object
+    const org = data.results.find((o) => o.name === req.body.organization);
+
+    return org.id;
   };
+
+  // Returns organization ID if org is newly created else searches
+  // for org data and returns existing ID
+  if (response.ok) {
+    console.log("Org successfully created");
+    return data.organization.id;
+  } else if (response.status === 422) {
+    return getExistingOrg();
+  } else {
+    res.json({ msg: `Error.. ${response.status}: ${response.statusText}` });
+  }
 };
 
 // Function to create or update user
-const createOrUpdateUser = async (user) => {
-  const res = user.res;
-  // User ID passed from checkUser.
-  const userId = user.users[0];
-  // URL to pdate or create user based on presence of userId
-  const url = `https://${subdomain}.zendesk.com/api/v2/users/${
-    userId ? userId.id : ""
-  }`;
+const createOrUpdateUser = async (org, req, res) => {
+  // URL to update or create user based on presence of userId
+  const url = `https://${subdomain}.zendesk.com/api/v2/users/create_or_update`;
 
   const config = {
     // Method to use based on presence of userID
-    method: `${userId ? "PUT" : "POST"}`,
-    headers: headers,
+    method: "POST",
+    headers,
     body: JSON.stringify({
       user: {
-        email: user.req.email,
-        name: user.req.name,
-        organization: {
-          name: `${user.req.organization}`,
-        },
+        email: req.body.email,
+        name: req.body.name,
         verified: true,
         tags: ["VIP"],
       },
     }),
   };
 
+  // Call to create or update user
   const response = await fetch(url, config);
+  const data = await response.json();
 
-  if (response.ok) {
+  return {
+    userId: data.user.id,
+    org,
+    res,
+  };
+};
+
+// Adds organization using the organization_memberships endpoint to account for multiple
+// organizations on a user profile.
+const updateOrg = async ({ userId, org, res }) => {
+  const url = `https://${subdomain}.zendesk.com/api/v2/organization_memberships`;
+
+  const config = {
+    method: "POST",
+    headers,
+    body: JSON.stringify({
+      organization_membership: {
+        user_id: userId,
+        organization_id: org,
+      },
+    }),
+  };
+
+  const response = await fetch(url, config);
+  const data = await response.json();
+
+  if (response.ok || response.status === 422) {
     res.redirect("/");
     console.log("Success");
   } else {
-    res.json({ msg: `Error.. ${response.status}: ${response.statusText}` });
+    console.log(data.details);
+    res.json({ msg: `Error.. ${response.status}: ${response.statusText}.` });
   }
 };
 
 app.post("/submit", (req, res) => {
-  // Creates Org if one does not exist
-  createOrg(req).then(() => {
-    // Checks if user exists
-    checkUser(req.body, res).then((data) => {
-      // Update or create user based on userId
-      createOrUpdateUser(data);
-    });
-  });
+  // Creates new Org or gets existing Org ID
+  createOrg(req)
+    // Create or update user based on userId
+    .then((data) => createOrUpdateUser(data, req, res))
+    // Add organization to user profile
+    .then((userData) => updateOrg(userData));
 });
 
 const PORT = process.env.PORT || 5000;
