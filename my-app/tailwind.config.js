4b7646414936df1eaf2cfb02b2e6ee723420fe03
Greg K <gkatechis@zendesk.com>
2022-01-12 22:33:20 -0600

    Responsive is working

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
+		   fontFamily: {
+      'sans': ['Poppins', 'system-ui'],
+      'serif': ['ui-serif', 'Georgia'],
+      'mono': ['IBM Plex Mono', 'SFMono-Regular']
+    },
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
 };
