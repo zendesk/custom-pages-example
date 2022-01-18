660b23af98000a7c54ff446f13e6bae4250a3380
Greg K <gkatechis@zendesk.com>
2022-01-18 11:10:21 -0600

    renamed main folder

---
+++ frontend/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    entry: './src/index.tsx',
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+  },
+};
