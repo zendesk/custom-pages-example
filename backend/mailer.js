20ebdb1f96ca4921f5007797f9ca68051171a8db
Greg K <gkatechis@zendesk.com>
2022-01-28 16:43:58 -0600

    Putting Tipene's work back together :awkward:

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,31 +1,29 @@
 const nodemailer = require('nodemailer');
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   notifyUser: (recipient) => {
-
     // Send via Gmail
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-    user: `${process.env.MAIL_SERVICE_USER}`,
-    pass: `${process.env.MAIL_SERVICE_PW}`
-  }
+        user: `${process.env.MAIL_SERVICE_USER}`,
+        pass: `${process.env.MAIL_SERVICE_PW}`,
+      },
     });
     const mailOptions = {
       from: 'vip.notifications1@gmail.com',
       to: recipient,
       subject: 'VIP Support',
-      text: 'Thanks for signing up for VIP Support!'
-      // message text - HTML to come
+      html: { path: './template.html' },
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
-    	console.log(error);
+        console.log(error);
       } else {
         console.log('Email sent: ' + info.response);
       }
     });
-  }
-}
+  },
+};
