081569e658b2f4a1b305dd6552157662d2eee231
Christopher Kennedy <chris.v.kennedy@gmail.com>
2022-01-28 15:28:40 -0500

    Include mail for user notifications

---
+++ backend/mailer.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer');
+require("dotenv").config();
+
+module.exports = {
+  notifyUser: (recipient) => {
+
+    // Send via Gmail
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+    user: `${process.env.MAIL_SERVICE_USER}`,
+    pass: `${process.env.MAIL_SERVICE_PW}`
+  }
+    });
+    const mailOptions = {
+      from: 'vip.notifications1@gmail.com',
+      to: recipient,
+      subject: 'VIP Support',
+      text: 'Thanks for signing up for VIP Support!'
+      // message text - HTML to come
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+    	console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
+}
