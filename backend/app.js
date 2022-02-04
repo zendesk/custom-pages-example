7ab9931a49746b8150cf78138574dc241d92883b
tipenehughes <tipenehughes@gmail.com>
2022-02-04 13:51:15 -0500

    Updated CORS header response

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 // Adding CORS headers to response
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next()
 });
 
